e7a535d2c2ecb045c393f0e771a318505e6e80ca
Jason <147480420+JsonSept@users.noreply.github.com>
2024-03-05 09:49:09 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -47,7 +47,7 @@ export default {
     return {
         show: false,
       greet: 'Hi',
-      name: 'Jason September nice to meet you',
+      name: 'Jason Septembe',
       text: 'And I am an',
       title: 'Aspiring Developer'
     
